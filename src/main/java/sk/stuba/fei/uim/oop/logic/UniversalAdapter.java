99b631ed1c64e477fc9024e0d5b7041d0f9a33ed
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-23 19:49:58 +0200

    vypracovanie

---
+++ src/main/java/sk/stuba/fei/uim/oop/logic/UniversalAdapter.java
@@ -0,0 +1,36 @@
+package sk.stuba.fei.uim.oop.logic;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class UniversalAdapter implements KeyListener, ActionListener, ChangeListener {
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+
+    }
+}
